daeb8000b58dfeb359dc4055efaef58e9a38d238
Darshan <darshan15aj@gmail.com>
2024-05-16 17:35:33 +0530

    media query issue

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -17,7 +17,7 @@ function Experience() {
       }}
       className='h-screen flex relative  overflow-hidden flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center'
     >
-      <h3 className='absolute top-16 uppercase tracking-[20px] text-gray-500 text-2xl'>
+      <h3 className='md:absolute md:top-16 uppercase tracking-[20px] text-gray-500 text-2xl'>
         Experience
       </h3>
 
